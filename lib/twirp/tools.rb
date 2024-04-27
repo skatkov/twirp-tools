23bc0eefe300c8152eeb36686bf5f75a183a8084
Stanislav (Stas) Katkov <git@skatkov.com>
2024-04-27 15:28:02 +0200

    blank gem

---
+++ lib/twirp/tools.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tools/version"
+
+module Twirp
+  module Tools
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/twirp/tools/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Twirp
+  module Tools
+    VERSION = "0.1.0"
+  end
+end
